744e41c5e1531a519d9a3eb412c0199b7c05b89d
Cerx <vojtech.duchacek@gmail.com>
2018-05-13 15:39:19 +0200

    Added missing bracket

--- commands/headDrop.js
+++ commands/headDrop.js
@@ -19,7 +19,7 @@ module.exports = {
 
     // Get a random UUID to ensure player's skin doesn't change.
       const randomUUID = await uuidv4()
-      const cmdString3 = '```' + `execute as @a[name=${name},scores={headDeath=1..}] run give @a[scores={headKill=1..}] player_head{display:{Name:"{"translate":"${name}'s Head","italic":false}",SkullOwner:{Id:"${randomUUID}",Properties:{textures:[{Value:"${userValue}"}]}}} 1` + '```'
+      const cmdString3 = '```' + `execute as @a[name=${name},scores={headDeath=1..}] run give @a[scores={headKill=1..}] player_head{display:{Name:"{"translate":"${name}'s Head","italic":false"},SkullOwner:{Id:"${randomUUID}",Properties:{textures:[{Value:"${userValue}"}]}}} 1` + '```'
 
       let logMessage = new Discord.RichEmbed()
             .setTitle(name + '\'s Head Command')
