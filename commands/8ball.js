62771e1e978a1cb3ca44899b9f6da659751f5d56
TomminMC <tomminmc@icloud.com>
2018-05-27 10:24:52 -0400

    test

--- commands/8ball.js
+++ commands/8ball.js
@@ -6,20 +6,13 @@ module.exports = {
   aliases: ['ask','q'],
   async execute (message, args) {
     try{
-
       if (!args[0]) {
-          const embed = new Discord.RichEmbed()
+          const logMessage = new Discord.RichEmbed()
               .setColor(0x00e6e6)
               .setDescription("You forgot to ask your question.. Try again with ?8ball [question]")
-          message.channel.send({
-              embed
-          });
-
+          message.channel.send(logMessage);
       } else {
-
-
-let AnswerMessage = message.client.eris.getRandomMessage('8ballCommand', 'okay')
-
+        let AnswerMessage = message.client.eris.getRandomMessage('8ballCommand', 'okay')
         let logMessage = new Discord.RichEmbed()
               .setTitle("8Ball Response")
               .setColor(0x00AAAA)
@@ -27,7 +20,6 @@ let AnswerMessage = message.client.eris.getRandomMessage('8ballCommand', 'okay')
 
               message.channel.send(logMessage);
       }
-
     }catch (e) {
       let logMessage = new Discord.RichEmbed()
         .setTitle("Can't Answer Question")
@@ -35,15 +27,6 @@ let AnswerMessage = message.client.eris.getRandomMessage('8ballCommand', 'okay')
         .setDescription(e)
       message.channel.send(message.client.eris.getRandomMessage('8ballCommand', 'error'), logMessage)
     }
-
-
-
-
-
-
-
-
-
-
   }
 }
+/* Author : Tommin */
